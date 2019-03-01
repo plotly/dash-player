07f0f96eef6b7efcfc67c5664771c43fa871a2c1
ycaokris <yi.cao2@mail.mcgill.ca>
2019-03-01 13:10:39 -0500

    Rebuild with setprops handling

--- dash_player/__init__.py
+++ dash_player/__init__.py
@@ -1,33 +1,46 @@
+from __future__ import print_function as _
+
 import os as _os
-import dash as _dash
 import sys as _sys
-from .version import __version__
+import json
+
+import dash as _dash
+
+# noinspection PyUnresolvedReferences
+from ._imports_ import *
+from ._imports_ import __all__
+
+if not hasattr(_dash, 'development'):
+    print('Dash was not successfully imported. '
+          'Make sure you don\'t have a file '
+          'named \n"dash.py" in your current directory.', file=_sys.stderr)
+    _sys.exit(1)
+
+_basepath = _os.path.dirname(__file__)
+_filepath = _os.path.abspath(_os.path.join(_basepath, 'package.json'))
+with open(_filepath) as f:
+    package = json.load(f)
+
+package_name = package['name'].replace(' ', '_').replace('-', '_')
+__version__ = package['version']
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
-
-_components = _dash.development.component_loader.load_components(
-    _os.path.join(_current_path, 'metadata.json'),
-    'dash_player'
-)
 
 _this_module = _sys.modules[__name__]
 
 
 _js_dist = [
     {
-        "relative_package_path": "bundle.js",
-        "external_url": (
-            "https://unpkg.com/dash-player@{}"
-            "/dash_player/bundle.js"
-        ).format(__version__),
-        "namespace": "dash_player"
+        'relative_package_path': 'dash_player.min.js',
+        'dev_package_path': 'dash_player.dev.js',
+        
+        'namespace': package_name
     }
 ]
 
 _css_dist = []
 
 
-for _component in _components:
-    setattr(_this_module, _component.__name__, _component)
-    setattr(_component, '_js_dist', _js_dist)
-    setattr(_component, '_css_dist', _css_dist)
+for _component in __all__:
+    setattr(locals()[_component], '_js_dist', _js_dist)
+    setattr(locals()[_component], '_css_dist', _css_dist)
--- usage-method.py
+++ usage-method.py
@@ -4,13 +4,13 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 
-app = dash.Dash('')
+app = dash.Dash(__name__)
 
 app.scripts.config.serve_locally = True
 
 
 app.layout = html.Div([
-    dash_player.Player(
+    dash_player.DashPlayer(
         id='video-player',
         url='http://media.w3.org/2010/05/bunny/movie.mp4',
         controls=True
